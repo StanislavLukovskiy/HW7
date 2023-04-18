aa14a685fd87de553d6f0d967b18f4f4672d9c97
stas <point.blank.point@bk.ru>
2023-04-18 22:11:40 +0300

    t2 almost

---
+++ t2/Program.cs
@@ -0,0 +1,70 @@
+﻿// Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+// и возвращает значение этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 1, 7 -> такого числа в массиве нет
+// 1, 1 -> 1
+
+int ReadInt(string message)
+{
+    System.Console.Write($"{message} > ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int[,] Generate2DArray(int cntRows, int cntColumns)
+{
+    int[,] array = new int[cntRows, cntColumns];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = i + j;
+        }
+    }
+    return array;
+}
+
+void Print2DArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write($"{array[i, j]}\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+(int, int) FindElement(int[,] array, int number)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (number == array[i, j])
+            {
+                return (i, j);
+            }
+        }
+    }
+    return (-1, -1);
+}
+
+int rows = ReadInt("Введите количество строк");
+int columns = ReadInt("Введите количество колонок");
+int[,] arr = Generate2DArray(rows, columns);
+Print2DArray(arr);
+int coordinate = ReadInt($"Введите номер строки для поиска  ");
+(int x, int y) = FindElement(arr, coordinate);
+if (x != -1)
+{
+    int nmb = 0;
+    System.Console.WriteLine($"Ваше число {nmb + 1}");
+}
+else
+{
+    System.Console.WriteLine("Такого эллемента нет в массиве");
+}
